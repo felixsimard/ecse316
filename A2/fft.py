691b48d2377dbfac2bd3bd3f898926586aaf25dc
CharlesBourbeau <bourbeau.charles99@gmail.com>
2021-03-18 19:49:06 -0400

    Implement DFT and FFT

--- A2/fft.py
+++ A2/fft.py
@@ -11,6 +11,9 @@ Group 40
 
 import sys
 from helper import *
+import numpy as np
+import numpy.fft
+from math import pi
 
 
 def parseCommandLineArgs():
@@ -38,6 +41,53 @@ def parseCommandLineArgs():
     return args
 
 
+def _dft(arr, k):
+    def gen():
+        N = len(arr)
+        for n in range(N):
+            xn = arr[n]
+            coef = np.exp(-1j * 2 * pi * k * n / N)
+
+            yield xn * coef
+
+    return np.sum(gen())
+
+
+def DFT(arr):
+    res = np.array([])
+    N = len(arr)
+    for k in range(N):
+        res = np.append(res, _dft(arr, k))
+
+    return res
+
+
+def FFT(arr: np.ndarray, threshold):
+    assert type(arr) is np.ndarray
+
+    def _fft(_arr, k):
+        if len(_arr) < threshold:
+            #print(_dft(_arr, k))
+            return _dft(_arr, k)
+        else:
+            N = len(_arr)
+            ns = set(range(N))
+            evens = set(i for i in ns if i % 2 == 0)
+            odds = ns - evens
+            evens, odds = np.array(list(evens)), np.array(list(odds))
+
+            Xeven = _fft(_arr[evens], k)
+            Xodd = _fft(_arr[odds], k)
+
+            return Xeven + np.exp(-1j*2*pi*k/N) * Xodd
+
+    res = np.array([])
+    for k in range(len(arr)):
+        res = np.append(res, _fft(arr, k))
+
+    return res
+
+
 def main():
     args = parseCommandLineArgs()
     print(args)
@@ -46,3 +96,23 @@ def main():
 # MAIN
 if __name__ == '__main__':
     main()
+
+    array = np.array([1,2,3,2])
+    print()
+    print('ours: ', FFT(array, 2))
+    print(DFT(array))
+    print('np: ', numpy.fft.fft(array))
+
+
+    # a = np.array([1,3,5,7])
+    # b = np.array([2,4,6,8])
+    #
+    # l = []
+    # for x, y in zip(a, b):
+    #     l.extend([x,y])
+    # print(l)
+    #
+    # c = [t1 for t1 in zip(a, b)]
+    # print(np.array(c))
+
+    # FFT([1,1,1,2,3,2,4,4,5,1])
